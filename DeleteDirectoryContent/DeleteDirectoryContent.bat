a729117a7f39e856af409820d17c866acf5870b8
Cole Cianflone <colecianflone123@gmail.com>
2022-05-16 18:09:44 -0500

    Update DeleteDirectoryContent.bat
    Added title for command prompt

--- DeleteDirectoryContent/DeleteDirectoryContent.bat
+++ DeleteDirectoryContent/DeleteDirectoryContent.bat
@@ -1,3 +1,4 @@
+TITLE DELETE-ALL-FILES-IN-DIRECTORY
 :: My first bat script which deletes all files in the current directory with a warning
 ::(Hidden, System, and Read-Only Files are Not Affected)
 @ECHO OFF
