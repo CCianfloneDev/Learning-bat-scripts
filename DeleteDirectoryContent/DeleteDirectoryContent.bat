5bda984272817ece2e5877f393d871d9f3be21a5
Cole Cianflone <colecianflone123@gmail.com>
2022-05-16 19:15:54 -0500

    Update DeleteDirectoryContent.bat

--- DeleteDirectoryContent/DeleteDirectoryContent.bat
+++ DeleteDirectoryContent/DeleteDirectoryContent.bat
@@ -1,5 +1,11 @@
+@ECHO OFF
 TITLE DELETE-ALL-FILES-IN-DIRECTORY
+
 :: My first bat script which deletes all files in the current directory with a warning
 ::(Hidden, System, and Read-Only Files are Not Affected)
-@ECHO OFF
+:: Logs the files from the current directory into a notepad and puts it in the parent directory.
+
+DIR ".\" > ..\ListOfDeletedFiles.txt
+
+ECHO files logged before deletion.
 DEL . DR
