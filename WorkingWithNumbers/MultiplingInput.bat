7eea964c949e5560517acca4e19609d7ef98155c
Cole Cianflone <colecianflone123@gmail.com>
2022-05-17 20:01:16 -0500

    Create MultiplingInput.bat

---
+++ WorkingWithNumbers/MultiplingInput.bat
@@ -0,0 +1,15 @@
+@ECHO off
+:: This batch script will take in two user input and multiply them"
+CLS
+
+ECHO Enter a number:
+set /p num1=
+
+ECHO Enter another number:
+set /p num2=
+
+set /a result=(%num1% * %num2%)
+
+CLS
+ECHO %num1% multiplied by %num2% = %result%
+PAUSE
