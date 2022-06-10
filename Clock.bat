c3e490ac12433584106a22b58750c84a6c6200d0
Cole Cianflone <colecianflone123@gmail.com>
2022-06-09 20:30:17 -0500

    Add files via upload

---
+++ Clock.bat
@@ -0,0 +1,14 @@
+@echo off
+Title Digital Clock
+@mode con cols=30 lines=7
+color 02
+
+:startClock
+cls
+echo.
+echo       Time: %time%
+echo.
+echo       Date: %date%
+echo.
+ping -n 2 0.0.0.0>nul
+goto startClock
